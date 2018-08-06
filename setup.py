f1f6e58a268ee14688cb96f6112d6b4b6423cac8
Julien Malard <julien.malard@mail.mcgill.ca>
2018-08-06 11:20:48 -0400

    ஒன்றும் இல்லை

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='ennikkai',
     version='1.0.0',
-    packages=['சோதனைகள்', 'எண்ணிக்கை'],
+    packages=find_packages(),
     url='https://ennikkai.readthedocs.io/ta/latest/',
-    license='GNU GLP 3',
-    author='ஜூலீஎன் மாலார் (Julien Jean Malard)',
+    license='GNU GPL 3',
+    author='ஜூலீஎன் ஜான் மாலார் (Julien Jean Malard)',
     author_email='julien.malard@mail.mcgill.ca',
     description='எண்களின் மொழிப்பெயர்ப்பு'
 )
